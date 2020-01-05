143a4ade56da1bbdd1539df53f335ea001a8976c
owenplanchart <58032109+owenplanchart@users.noreply.github.com>
2020-01-05 14:26:41 +0000

    AllegoryCaveMacBook

---
+++ Steam VR Second Round/Assets/Scripts/DisplacementScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisplacementScript : MonoBehaviour
+{
+    public float displacementAmount;
+    //public ParticleSystem explosionParticles;
+    MeshRenderer meshRender;
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRender = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime);
+        meshRender.material.SetFloat("_Amount", displacementAmount);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            displacementAmount += 0.01f;
+            //explosionParticles.Play();
+        }
+    }
+}
